0a418bc4d70a555fa5f70fe6a75b382f15b3fd25
Sergio Mancera <sergio@MacBook-Air-de-Sergio.local>
2018-03-17 16:21:41 -0500

    Added draw line test cases

--- draw.py
+++ draw.py
@@ -8,7 +8,7 @@ class Canvas():
     _quit   = False
 
     def __init__(self):
-        self._canvas = np.zeros(shape = (0,0))
+        self._canvas = np.zeros((0,0), str)
         self._quit = False
 
     def is_canvas(self):
@@ -20,17 +20,16 @@ class Canvas():
         return self._quit
 
     def execute(self, command):
-        match = re.match("([CLRBQclrbq])\s?(\d)?\s?(\d)?\s?(\w)?\s?(\d)?", command)
+        match = re.match("([CLRBQclrbq])\s*(\d*)?\s*(\d*)?\s*(\w*)?\s*(\d*)?", command)
         if match:
             if match.group(1).lower() == 'c':
                 try:
                     w = int(match.group(2))
                     h = int(match.group(3))
+                    self.create(w,h)
                     return True
                 except:
-                    print('Invalid parameters for C')
-                    print('Usage: C w h, where both w and h are integers, the former is the width and h is the height')
-                    return False
+                    return "CINV"
             if match.group(1).lower() == 'l':
                 pass
             if match.group(1).lower() == 'r':
@@ -41,13 +40,35 @@ class Canvas():
                 pass
         return 'NOP'
     def size(self):
-        return self._canvas.shape
+        h, w = self._canvas.shape
+        return (w, h)
     def create(self, w, h):
-        self._canvas = np.zeros(shape = (w,h))
+        self._canvas = np.zeros((h, w), str)
+        self._canvas[1][2] = 'x'
+        self._canvas[1][3] = 'x'
+        self._canvas[1][4] = 'x'
+        self._canvas[1][5] = 'x'
 
     def draw_canvas(self):
         if self.is_canvas():
-            pass
+            w, h = self.size()
+            for x in range(0, h+2):
+                if x == 0 or x == h+1:
+                    print('-', end = '')
+                else:
+                    print('|', end = '')
+                for y in range(0,w):
+                    if x == 0 or x == h+1:
+                        print('-', end = '')
+                    else:
+                        character = self._canvas[x][y]
+                        print(self._canvas[x][y], end = '')
+                if x == 0 or x == h+1:
+                    print('-')
+                else:
+                    print('|')
+            print()
+            print(self._canvas)
 
 
 if __name__ == "__main__":
@@ -64,5 +85,11 @@ if __name__ == "__main__":
             quit = True
         inp = input('enter command: ')
 
-        canvas.execute(inp)
+        result = canvas.execute(inp)
+        if result == True:
+            pass
+        else:
+            if result == "CINV":
+                print('Invalid parameters for C')
+                print('Usage: C w h, where both w and h are integers, the former is the width and h is the height')
         canvas.draw_canvas()
--- test_draw.py
+++ test_draw.py
@@ -1,5 +1,8 @@
 import unittest
+import numpy as np
 from draw import Canvas
+
+
 
 class DrawTestCase(unittest.TestCase):
     """Tests for `draw.py`."""
@@ -10,18 +13,54 @@ class DrawTestCase(unittest.TestCase):
     def test_create_canvas(self):
         """It is possible to create a canvas?"""
 
-        #test errors
-        self.assertFalse(self.canvas.execute("C"))
-        self.assertFalse(self.canvas.execute("C z"))
-        self.assertFalse(self.canvas.execute("C z z"))
-        self.assertFalse(self.canvas.execute("C z 2"))
-        self.assertFalse(self.canvas.execute("C 2 a"))
+        #test input errors
+        self.assertEqual(self.canvas.execute("C"), "CINV")
+        self.assertEqual(self.canvas.execute("C z"), "CINV")
+        self.assertEqual(self.canvas.execute("C z z"), "CINV")
+        self.assertEqual(self.canvas.execute("C z 2"), "CINV")
+        self.assertEqual(self.canvas.execute("C 2 z"), "CINV")
         
         #test valid creation
         self.assertTrue(self.canvas.execute("C 3 3"))
         self.assertEqual(self.canvas.size(), (3,3))
         self.assertTrue(self.canvas.execute("C 10 20"))
         self.assertEqual(self.canvas.size(), (10,20))
+        self.assertTrue(self.canvas.execute("C 10 2  "))
+        self.assertEqual(self.canvas.size(), (10,2))
+        self.assertTrue(self.canvas.execute("C  1  2   "))
+        self.assertEqual(self.canvas.size(), (1,2))
+
+    def test_draw_line(self):
+        """Lets you draw a straight line?"""
+
+        #test input errors
+        self.assertEqual(self.canvas.execute("L"), "LINV")
+        self.assertEqual(self.canvas.execute("L x x x x"), "LINV")
+        self.assertEqual(self.canvas.execute("L x 2 3 4"), "LINV")
+        self.assertEqual(self.canvas.execute("L 23 4 2 x"), "LINV")
+        self.assertEqual(self.canvas.execute("L 2 2 2 x"), "LINV")
+
+        #test out of range errors
+        self.canvas.execute("C 3 3")
+        self.assertEqual(self.canvas.execute("L 0 0 0 3"), "LRANGE")
+
+        #test not straight lines
+        self.canvas.execute("C 9 9")
+        self.assertEqual(self.canvas.execute("L 1 1 5 5"), "LSTR8L")
+
+        #test line drawing
+        self.canvas.execute("C 9 9")
+        testarray = np.zeros((9, 9), str)
+
+        testarray[1][1] = 'x'
+        testarray[1][2] = 'x'
+        testarray[1][3] = 'x'
+        testarray[1][4] = 'x'
+        testarray[1][5] = 'x'
+
+
+        self.canvas.execute("L 1 1 1 5")
+        self.assertEqual(self.canvas.get_canvas(), testarray)
 
 if __name__ == '__main__':
     unittest.main()
